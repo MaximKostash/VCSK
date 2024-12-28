befb6bdddb4b7b68ffa0011a84cbf60741bf0a5d
Max Kostash <maxkostash0@gmail.com>
2024-12-28 19:57:44 +0200

    Create init_f_info_file.c

---
+++ src/init_f_info_file.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int INIT_F_INFO_FILE()                                 
+{
+    GError *error = NULL;                                  
+    if (g_file_set_contents(init_c.info_file, init_c.content_info_file, -1, &error))                              
+    {
+        printf("file...\n");
+    }
+    else
+    {
+        printf("Error.\n");
+        g_error_free(error);
+    }
+    return 0;
+}
