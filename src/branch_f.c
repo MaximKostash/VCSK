19737149c928fe2af90280af02c24fad109e0c1a
Max Kostash <maxkostash0@gmail.com>
2024-12-28 19:50:47 +0200

    Create branch_f.c

---
+++ src/branch_f.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int BRANCH_F(const gchar *branch_name)                 
+{
+    gchar *branch_path = g_strdup_printf("%s/%s", init_c.ver_project, branch_name);                               
+    if (g_mkdir_with_parents(branch_path, 0775) == 0)      
+    {
+        printf("branch...\n");
+    }
+    else
+    {
+        perror("Error.\n");
+        g_free(branch_path);
+        return 1;
+    }
+    g_free(branch_path);
+    return 0;
+}
