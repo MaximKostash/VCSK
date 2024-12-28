40ec48831ccbc71e59956d7f9248492b969628ff
Max Kostash <maxkostash0@gmail.com>
2024-12-28 19:56:13 +0200

    Create init_f_info.c

---
+++ src/init_f_info.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"                            
+
+int INIT_F_INFO()                                      
+{
+    if (g_mkdir_with_parents(init_c.info, 0775) == 0)      
+    {                                                          
+      printf("info...\n");                               
+    }
+    else
+    {
+        perror("Error.\n");
+        return 1;
+    }
+    return 0;
+}
