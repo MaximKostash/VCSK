573cf90da08ac8dc953d12cad07eedfd0415bfdc
Max Kostash <maxkostash0@gmail.com>
2024-12-28 19:59:15 +0200

    Create init_f_init.c

---
+++ src/init_f_init.c
@@ -0,0 +1,17 @@
+#include <stdio.h>                                     
+#include <glib.h>
+#include "../include/fun.h"
+
+int INIT_F_INIT()                                      
+{
+    if (g_mkdir_with_parents(init_c.init, 0775) == 0)
+    {
+        printf("init...\n");
+    }
+    else
+    {
+        perror("Error.\n");
+        return 1;
+    }
+    return 0;
+}
