f1ea7f060938c409a3c63980cc9a287137702b80
Max Kostash <maxkostash0@gmail.com>
2024-12-29 12:09:34 +0200

    Add files via upload

---
+++ src/init_f.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+void INIT_F()
+{
+    if (g_file_test(init_c.init, G_FILE_TEST_IS_DIR))
+    {
+        printf("init\n");
+    }
+    else
+    {
+        INIT_F_INIT();
+        INIT_F_INFO();
+        INIT_F_INFO_FILE();
+        INIT_F_MAIN();
+        INIT_F_VER_PROJECT();
+    }
+}
