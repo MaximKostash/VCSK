2315fb7f47fd55044cf058c6b2e8ae5346207a95
Max Kostash <maxkostash0@gmail.com>
2024-12-29 11:55:02 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <glib.h>
-#include "include/fun.h"                                                                                     
+#include "include/fun.h"
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        printf("vcsk <command>\n");                        
-    }                                                      
+        printf("vcsk <command>\n");
+    }
     if (strcmp(argv[1], "init") == 0)
     {
-        INIT_F();                                          
+        INIT_F();
     }
     else if (strcmp(argv[1], "branch") == 0 && argc == 3)
     {
-        if (g_file_test(init_c.init, G_FILE_TEST_IS_DIR))                                                             
+        if (g_file_test(init_c.init, G_FILE_TEST_IS_DIR))
         {
             BRANCH_F(argv[2]);
         }
