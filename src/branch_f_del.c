01513708a9b81f4fea6aa426f8d4b2a0f269bfdf
Max Kostash <maxkostash0@gmail.com>
2025-01-03 15:32:09 +0200

    Add files via upload

---
+++ src/branch_f_del.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int BRANCH_F_DEL(const gchar *file)
+{
+    gchar *d = g_strdup_printf("%s/%s", init_c.main, file);
+    if (g_file_test(d, G_FILE_TEST_EXISTS))
+    {
+        if (remove(d) == 0)
+        {
+            printf("delete...\n");
+        }
+        else
+        {
+            printf("Error.\n");
+        }
+    }
+    return 0;
+}
---
+++ src/branch_f_del_dir.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int BRANCH_F_DEL_DIR(const gchar *dir, const gchar *file)
+{
+    gchar *d = g_strdup_printf("%s/%s/%s", init_c.ver_project, dir, file);
+    if (g_file_test(d, G_FILE_TEST_EXISTS))
+    {
+        if (remove(d) == 0)
+        {
+            printf("delete...\n");
+        }
+        else
+        {
+            printf("Error.\n");
+        }
+    }
+    return 0;
+}
