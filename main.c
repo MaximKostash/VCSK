d76f5fa7cf3522f5f7c839b3f418c95f36c88d0b
Max Kostash <maxkostash0@gmail.com>
2025-01-04 15:58:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -115,5 +115,16 @@ int main(int argc, char *argv[])
             printf("init\n");
         }
     }
+    else if (strcmp(argv[1], "del-ver") == 0 && argc == 3)
+    {
+        if (g_file_test(init_c.init, G_FILE_TEST_IS_DIR))
+        {
+            BRANCH_F_DEL_VER(argv[2]);
+        }
+        else
+        {
+            printf("init\n");
+        }
+    }
     return 0;
 }
