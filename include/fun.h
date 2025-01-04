d8c793eabf4cb5b2e31612765021dd646423e5ca
Max Kostash <maxkostash0@gmail.com>
2025-01-04 15:59:51 +0200

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -31,5 +31,6 @@ int BRANCH_F_DEL(const gchar *file);
 int BRANCH_F_DEL_DIR(const gchar *dir, const gchar *file);
 int BRANCH_F_NOTES(const gchar *notes_file);
 int BRANCH_F_NOTES_CLEAR();
+int BRANCH_F_DEL_VER(const gchar *dir);
 
 #endif
