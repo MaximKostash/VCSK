ce1d8cda4d3eb6bb1892f42705de00cddf849d2e
Max Kostash <maxkostash0@gmail.com>
2025-01-04 12:48:18 +0200

    Add files via upload

---
+++ src/branch_f_notes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int BRANCH_F_NOTES(const gchar *notes_file)
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    gchar *file_name = g_strdup_printf("Notes-%d-%d-%d-%d-%d.txt", timeinfo->tm_min, timeinfo->tm_hour, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
+    gchar *d = g_strdup_printf("%s/%s", init_c.notes, file_name);
+    GError *error = NULL;
+    if (g_file_set_contents(d, notes_file, -1, &error))
+    {
+        printf("notes...\n");
+    }
+    else
+    {
+        printf("Error.\n");
+        g_error_free(error);
+    }
+    return 0;
+}
---
+++ src/init_f_notes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <glib.h>
+#include "../include/fun.h"
+
+int INIT_F_NOTES()
+{
+    if (g_mkdir_with_parents(init_c.notes, 0775) == 0)
+    {
+        printf("notes...\n");
+    }
+    else
+    {
+        perror("Error.\n");
+        return 1;
+    }
+    return 0;
+}
