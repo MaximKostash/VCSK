8538cb31b46046bad22605efe256afdc7459a407
Max Kostash <maxkostash0@gmail.com>
2025-01-04 12:42:47 +0200

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -10,6 +10,7 @@ typedef struct {
     const gchar *content_info_file;
     const gchar *main;
     const gchar *ver_project;
+    const gchar *notes;
 } INIT;
 
 extern INIT init_c;
@@ -20,6 +21,7 @@ int INIT_F_INFO();
 int INIT_F_INFO_FILE();
 int INIT_F_MAIN();
 int INIT_F_VER_PROJECT();
+int INIT_F_NOTES();
 int BRANCH_F(const gchar *branch_name);
 int BRANCH_F_ADD(const gchar *file_name);
 int BRANCH_F_ADD_DIR(const gchar *dir, const gchar *file);
@@ -27,5 +29,7 @@ int BRANCH_F_REPLACE(const gchar *file);
 int BRANCH_F_REPLACE_DIR(const gchar *dir, const gchar *file);
 int BRANCH_F_DEL(const gchar *file);
 int BRANCH_F_DEL_DIR(const gchar *dir, const gchar *file);
+int BRANCH_F_NOTES(const gchar *notes_file);
+int BRANCH_F_NOTES_CLEAR();
 
 #endif
