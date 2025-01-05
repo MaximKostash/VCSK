c1efd2a99dba897fc77952f60a72bb65e766e487
Max Kostash <maxkostash0@gmail.com>
2025-01-05 13:49:04 +0200

    Update init_f_notes.c

--- src/init_f_notes.c
+++ src/init_f_notes.c
@@ -6,7 +6,7 @@ int INIT_F_NOTES()
 {
     if (g_mkdir_with_parents(init_c.notes, 0775) == 0)
     {
-        printf("notes...\n");
+        printf("Performing the operation...\n");
     }
     else
     {
