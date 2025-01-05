a484466041f38eca003e7b8ab3a6378a3801aa23
Max Kostash <maxkostash0@gmail.com>
2025-01-05 13:36:35 +0200

    Update init_data.c

--- src/init_data.c
+++ src/init_data.c
@@ -1,11 +1,11 @@
 #include "../include/fun.h"
 
 INIT init_c = {
-    ".VCSK",
-    ".VCSK/info",
-    ".VCSK/info/info.txt",
-    "Hello, world!\n",
-    ".VCSK/main",
-    ".VCSK/ver_project",
-    ".VCSK/notes"
+    ".vcsk",
+    ".vcsk/info",
+    ".vcsk/info/info.txt",
+    "Please provide information about your project in this file.\n",
+    ".vcsk/main",
+    ".vcsk/ver_project",
+    ".vcsk/notes"
 };
