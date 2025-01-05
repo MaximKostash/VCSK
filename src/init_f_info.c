8152b54091c1d6b56235de56561716fa2e41bddc
Max Kostash <maxkostash0@gmail.com>
2025-01-05 13:47:59 +0200

    Update init_f_info.c

--- src/init_f_info.c
+++ src/init_f_info.c
@@ -6,7 +6,7 @@ int INIT_F_INFO()
 {
     if (g_mkdir_with_parents(init_c.info, 0775) == 0)
     {
-        printf("info...\n");
+        printf("Performing the operation...\n");
     }
     else
     {
