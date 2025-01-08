1fa99a04af5947612a2019834ce1b49c555517b1
Max Kostash <maxkostash0@gmail.com>
2025-01-08 19:43:53 +0200

    Update init_f_info_file.c

--- src/init_f_info_file.c
+++ src/init_f_info_file.c
@@ -7,7 +7,7 @@ int INIT_F_INFO_FILE()
     GError *error = NULL;
     if (g_file_set_contents(init_c.info_file, init_c.content_info_file, -1, &error))
     {
-        comm.c2;
+        printf("%s", comm.c2);
     }
     else
     {
