13bdd2da35730636d6f7a6908677247988fceebf
Max Kostash <maxkostash0@gmail.com>
2025-01-08 16:22:12 +0200

    Update init_f_info_file.c

--- src/init_f_info_file.c
+++ src/init_f_info_file.c
@@ -7,7 +7,7 @@ int INIT_F_INFO_FILE()
     GError *error = NULL;
     if (g_file_set_contents(init_c.info_file, init_c.content_info_file, -1, &error))
     {
-        printf("Performing the operation...\n");
+        comm.c2;
     }
     else
     {
