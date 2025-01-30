a57b24088e40e0370e7e67c45dc20a56e95fc07b
Max Kostash <maxkostash0@gmail.com>
2025-01-30 15:35:38 +0200

    Update help.c

--- src/help.c
+++ src/help.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "../include/fun.h"
 
 int HELP()
@@ -6,15 +5,15 @@ int HELP()
     printf("HELP\n\n");
     printf("ver - The name of your project.\nfile - The name of your file.\ndir - The name of your directory.\n\n");
     printf("vcsk init - system initialization\n");
-    printf("vcsk branch <ver> - creating a branch\n");
+    printf("vcsk branch <dir> - creating a branch\n");
     printf("vcsk add <file> - adding to main folder\n");
-    printf("vcsk add-dir <ver> <file> - adding a version to a specific folder\n");
+    printf("vcsk add-dir <dir> <file> - adding a version to a specific folder\n");
     printf("vcsk replace <file> - replace with file from main folder\n");
-    printf("vcsk replace-dir <ver> <file> - replace with file from specific version folder\n");
+    printf("vcsk replace-dir <dir> <file> - replace with file from specific version folder\n");
     printf("vcsk del <file> - deleting a file from the main folder\n");
-    printf("vcsk del-dir <ver> <file> - delete file from specific version folder\n");
+    printf("vcsk del-dir <dir> <file> - delete file from specific version folder\n");
     printf("vcsk notes \"text\" - make a note\n");
     printf("vcsk notes-clear - clears all the notes\n");
-    printf("vcsk del-ver <ver> - delete specific version folder\n");
+    printf("vcsk del-ver <dir> - delete specific version folder\n");
     return 0;
 }
