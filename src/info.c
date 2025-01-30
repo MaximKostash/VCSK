64005e2802a1da27699aa089d46c4e8d25e8dc00
Max Kostash <maxkostash0@gmail.com>
2025-01-30 15:39:00 +0200

    Update info.c

--- src/info.c
+++ src/info.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "../include/fun.h"
 
 int INFO()
 {
     printf("INFO\n\n");
-    printf("It is an easy to use Linux based\nsystem that will help you manage\nyour project.\n\n");
+    printf("It is an easy to use system that will\nhelp you manage your project.\n\n");
     printf("Email: maxkostash0@gmail.com\n");
     return 0;
 }
