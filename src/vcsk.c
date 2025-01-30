e879da055ea4b051a768e52b209e58de564ff2fb
Max Kostash <maxkostash0@gmail.com>
2025-01-30 15:41:36 +0200

    Update vcsk.c

--- src/vcsk.c
+++ src/vcsk.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "../include/fun.h"
 
 int VCSK()
 {
-    printf("VCSK 1.7.1\n\n");
+    printf("VCSK 1.8.0\n\n");
     printf("Commands:\n");
     printf("vcsk help - help\n");
     printf("vcsk info - info\n");
